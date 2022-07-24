ab3590f84e62ddc7a85fce5ab70a92f697864ef5
alexcoder04 <alexcoder04@protonmail.com>
2022-07-24 19:28:34 +0200

    initial code

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+
+	"github.com/alexcoder04/arrowprint"
+	"github.com/alexcoder04/friendly"
+	"github.com/alexcoder04/gilc"
+)
+
+func pcommand(data gilc.IData, args []string) {
+	if len(args) < 1 {
+		arrowprint.Err0("you need to pass a url to download")
+		return
+	}
+
+	dlDir := getDownloadDir(data)
+	if !friendly.IsDir(dlDir) {
+		err := os.MkdirAll(dlDir, 0700)
+		if err != nil {
+			arrowprint.Err0("cannot create download folder: %s", err.Error())
+			return
+		}
+	}
+
+	ok := false
+	for i, u := range args {
+		arrowprint.Info0("downloading file %d of %d: '%s'...", i, len(args), u)
+		err := download(u, dlDir)
+		if err == nil {
+			ok = true
+		}
+	}
+
+	if ok {
+		arrowprint.Info0("your file(s) have been downloaded to %s", dlDir)
+	}
+}
+
+func main() {
+	gilc.Setup("download file from it's url", func(i gilc.IData) {}, pcommand, func(i gilc.IData) {})
+	gilc.Run()
+}
---
+++ utils.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/url"
+	"path"
+
+	"github.com/alexcoder04/arrowprint"
+	"github.com/alexcoder04/friendly"
+	"github.com/alexcoder04/gilc"
+)
+
+func getDownloadDir(data gilc.IData) string {
+	user := data.Username
+	if user == "" || user == "/" {
+		user = "_"
+	}
+	return path.Join(data.SavePath, "home", user, "downloads")
+}
+
+func download(u string, dlDir string) error {
+	URL, err := url.Parse(u)
+	if err != nil {
+		arrowprint.Err0("invalid url: '%s'", u)
+		return err
+	}
+
+	fileName := path.Base(URL.Path)
+	if fileName == "" || fileName == "/" {
+		fileName = "index.html"
+	}
+	err = friendly.DownloadFile(u, path.Join(dlDir, fileName))
+	if err != nil {
+		arrowprint.Err0("download for '%s' failed: %s", u, err.Error())
+		return err
+	}
+
+	arrowprint.Suc1("downloaded '%s' successfully", u)
+	return nil
+}
